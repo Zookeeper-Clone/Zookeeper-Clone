88d54d858039ac1eaa6222e06eaf8c722ab60759
Abdelrahman-Nasr6161 <abdoahlawy6161@gmail.com>
2025-11-20 19:49:21 +0200

    extracted redundancy in zookeeperClient initialization in TestStore.java using @BeforeEach

--- client/src/test/java/TestStore.java
+++ client/src/test/java/TestStore.java
@@ -1,84 +1,93 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertSame;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import client.zookeeper.ZookeeperClient;
 
 public class TestStore {
+
+    private MockRaftClient mockRaftClient;
+    private ZookeeperClient zookeeperClient;
+
+    @BeforeEach
+    public void setUp() {
+        mockRaftClient = new MockRaftClient();
+        zookeeperClient = new ZookeeperClient(mockRaftClient);
+    }
+
     @Test
-    public void addEntry(){
-        MockRaftClient mockRaftClient = new MockRaftClient();
-        ZookeeperClient zookeeperClient = new ZookeeperClient(mockRaftClient);
+    public void addEntry() {
         String key = "key";
         String value = "value";
         String response = zookeeperClient.write(key, value);
         assertEquals("OK ENTRY ADDED", response);
     }
+
     @Test
-    public void readEntry(){
-        MockRaftClient mockRaftClient = new MockRaftClient();
-        ZookeeperClient zookeeperClient = new ZookeeperClient(mockRaftClient);
+    public void readEntry() {
         String key = "key";
         String value = "value";
         zookeeperClient.write(key, value);
         String response = zookeeperClient.read(key);
-        assertEquals(response, "value");
+        assertEquals("value", response);
     }
+
     @Test
-    public void deleteEntry(){
-        MockRaftClient mockRaftClient = new MockRaftClient();
-        ZookeeperClient zookeeperClient = new ZookeeperClient(mockRaftClient);
+    public void deleteEntry() {
         String key = "key";
         String value = "value";
         zookeeperClient.write(key, value);
         boolean deleteResponse = zookeeperClient.delete(key);
         assert(deleteResponse);
         String response = zookeeperClient.read(key);
-        assertEquals(response, "KEY DOESN'T EXIST");
+        assertEquals("KEY DOESN'T EXIST", response);
     }
+
     @Test
-    public void multiWriteEntry(){
-        MockRaftClient mockRaftClient = new MockRaftClient();
-        ZookeeperClient zookeeperClient = new ZookeeperClient(mockRaftClient);
+    public void multiWriteEntry() {
         String key1 = "key1";
         String value1 = "value1";
         String writeResponse1 = zookeeperClient.write(key1, value1);
+
         String key2 = "key2";
         String value2 = "value2";
         String writeResponse2 = zookeeperClient.write(key2, value2);
+
         String readResponse1 = zookeeperClient.read(key1);
         String readResponse2 = zookeeperClient.read(key2);
-        assertEquals(writeResponse1, "OK ENTRY ADDED");
-        assertEquals(writeResponse2, "OK ENTRY ADDED");
-        assertEquals(readResponse1, "value1");
-        assertEquals(readResponse2, "value2");
+
+        assertEquals("OK ENTRY ADDED", writeResponse1);
+        assertEquals("OK ENTRY ADDED", writeResponse2);
+        assertEquals("value1", readResponse1);
+        assertEquals("value2", readResponse2);
     }
+
     @Test
-    public void updateEntry(){
-        MockRaftClient mockRaftClient = new MockRaftClient();
-        ZookeeperClient zookeeperClient = new ZookeeperClient(mockRaftClient);
+    public void updateEntry() {
         String key1 = "key1";
         String value1 = "value1";
         zookeeperClient.write(key1, value1);
+
         value1 = "newValue1";
         String response = zookeeperClient.write(key1, value1);
-        assertEquals(response, "OK ENTRY UPDATED");
+        assertEquals("OK ENTRY UPDATED", response);
+
         String readResponse = zookeeperClient.read(key1);
-        assertEquals(readResponse, value1);
+        assertEquals(value1, readResponse);
     }
+
     @Test
-    public void displayAllEntry()
-    {
-        MockRaftClient mockRaftClient = new MockRaftClient();
-        ZookeeperClient zookeeperClient = new ZookeeperClient(mockRaftClient);
+    public void displayAllEntry() {
         String key1 = "key1";
         String value1 = "value1";
         zookeeperClient.write(key1, value1);
+
         String key2 = "key2";
         String value2 = "value2";
         zookeeperClient.write(key2, value2);
+
         String readAll = zookeeperClient.readAll();
-        assertEquals(readAll, "key1 : value1\nkey2 : value2\n");
+        assertEquals("key1 : value1\nkey2 : value2\n", readAll);
     }
 }
